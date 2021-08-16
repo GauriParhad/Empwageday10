9ae49092ace841ceddc7edf33509f4bc744ca34c
Gaurip10 <thegauriparhad@gmail.com>
2021-08-16 10:09:05 +0530

    Added: adding part time

--- src/Employeewage.java
+++ src/Employeewage.java
@@ -1,24 +1,26 @@
 public class Employeewage {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation program");
 
         //constants
-        int IS_FULL_TIME=1;
-        int EMP_RATE_PER_HOUR=20;
+        int IS_FULL_TIME = 2;
+        int EMP_RATE_PER_HOUR = 20;
+        int IS_PART_TIME = 1;
 
         //variables
-        int emphrs=0;
-        int empwage=0;
+        int emphrs = 0;
+        int empwage = 0;
 
         //Computation
-        double empcheck =Math.floor(Math.random() * 10)%2;
-        if (empcheck == IS_FULL_TIME)
-            emphrs=8;
+        double empcheck = Math.floor(Math.random() * 10) % 3;
+        if (empcheck == IS_PART_TIME)
+            emphrs = 4;
+        else if (empcheck == IS_FULL_TIME)
+            emphrs = 8;
         else
-            emphrs=0;
-        empwage= emphrs * EMP_RATE_PER_HOUR
-        System.out.println("Employee wage:"+empwage);
-
+            emphrs = 0;
+        empwage = emphrs * EMP_RATE_PER_HOUR
+        System.out.println("Employee wage:" + empwage);
     }
 }
 
