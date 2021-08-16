c9c40aa2eb4fe4e22fd29c3136e86e9b95035b61
Gaurip10 <thegauriparhad@gmail.com>
2021-08-16 10:02:18 +0530

    Added:Check emp presence

--- src/Employeewage.java
+++ src/Employeewage.java
@@ -1,6 +1,13 @@
 public class Employeewage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation program");
+        //constants
+        int IS_FULL_TIME=1;
+        //Computation
+        double empcheck =Math.floor(Math.random() * 10)%2;
+        if (empcheck == IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
-
 }
