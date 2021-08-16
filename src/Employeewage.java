c9bf0fd2c8e875626e42a5808171e3a471b68234
Gaurip10 <thegauriparhad@gmail.com>
2021-08-16 10:10:44 +0530

    Added: By using switch case

--- src/Employeewage.java
+++ src/Employeewage.java
@@ -1,26 +1,30 @@
 public class Employeewage {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee wage computation program");
+    public static final int IS_FULL_TIME=2;
+    public static final int EMP_RATE_PER_HOUR=20;
+    public static final int IS_PART_TIME=1;
 
-        //constants
-        int IS_FULL_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
-        int IS_PART_TIME = 1;
+    public static void main(String[] args){\
 
         //variables
-        int emphrs = 0;
-        int empwage = 0;
+        int emphrs=0;
+        int empwage=0;
 
         //Computation
-        double empcheck = Math.floor(Math.random() * 10) % 3;
-        if (empcheck == IS_PART_TIME)
-            emphrs = 4;
-        else if (empcheck == IS_FULL_TIME)
-            emphrs = 8;
-        else
-            emphrs = 0;
-        empwage = emphrs * EMP_RATE_PER_HOUR
-        System.out.println("Employee wage:" + empwage);
+        int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empcheck=0) {
+            case  IS_PART_TIME:
+                emphrs=4;
+                break;
+            case IS_FULL_TIME:
+                emphrs=8;
+                break;
+            default:
+                emphrs=0;
+        }
+        empwage= emphrs * EMP_RATE_PER_HOUR
+        System.out.println("Employee wage:"+empwage);
+
     }
 }
 
+    
