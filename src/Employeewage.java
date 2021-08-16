255f1fc1acaf8ee14dd61932823f46955630be96
Gaurip10 <thegauriparhad@gmail.com>
2021-08-16 10:06:45 +0530

    Added: Calculate employee wage

--- src/Employeewage.java
+++ src/Employeewage.java
@@ -1,13 +1,24 @@
 public class Employeewage {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Welcome to Employee wage computation program");
+
         //constants
         int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HOUR=20;
+
+        //variables
+        int emphrs=0;
+        int empwage=0;
+
         //Computation
         double empcheck =Math.floor(Math.random() * 10)%2;
         if (empcheck == IS_FULL_TIME)
-            System.out.println("Employee is Present");
+            emphrs=8;
         else
-            System.out.println("Employee is Absent");
+            emphrs=0;
+        empwage= emphrs * EMP_RATE_PER_HOUR
+        System.out.println("Employee wage:"+empwage);
+
     }
 }
+
